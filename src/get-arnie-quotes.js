79f0c37f36367bfc29cb82d1dd013e4780c99dbb
weipeng-jiang <weipengjiang1@gmail.com>
2025-11-03 08:38:06 +1100

    format

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,33 +1,33 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 // add more statuses if needed
 const STATUS = {
-  FULFILLED: 'fulfilled',
-}
+  FULFILLED: "fulfilled",
+};
 
 // add more status codes if needed
 const STATUS_CODE = {
   SUCCESS: 200,
-}
+};
 
 const getArnieQuotes = async (urls) => {
-    const responses = await Promise.allSettled(urls.map((url) => httpGet(url)));
+  const responses = await Promise.allSettled(urls.map((url) => httpGet(url)));
 
-    const results = responses.map((res) => {
-      if (res.status === STATUS.FULFILLED) {
-        const { status, body } = res.value;
-        const { message } = JSON.parse(body);
+  const results = responses.map((res) => {
+    if (res.status === STATUS.FULFILLED) {
+      const { status, body } = res.value;
+      const { message } = JSON.parse(body);
 
-        return status === STATUS_CODE.SUCCESS
-          ? { 'Arnie Quote': message }
-          : { FAILURE: message };
-      }
+      return status === STATUS_CODE.SUCCESS
+        ? { "Arnie Quote": message }
+        : { FAILURE: message };
+    }
 
-      // In case httpGet rejects, handle it here
-      // and return failure message
-    });
+    // In case httpGet rejects, handle it here
+    // and return failure message
+  });
 
-    return results;
+  return results;
 };
 
 module.exports = {
